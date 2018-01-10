d7ba3edc1d38a52f89c3b42e9ad58db813b603c5
Anthony Najjar Simon <anthony.najjar.simon@stylight.com>
2018-01-10 18:17:43 +0100

    Fix FileSaver isssue

--- web-app/src/app/locales/services/locales.service.ts
+++ web-app/src/app/locales/services/locales.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import FileSaver from 'file-saver';
+import * as FileSaver from 'file-saver';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
