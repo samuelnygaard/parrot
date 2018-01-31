af4c69a83f46c9544bbdcc464eccc05c48562d8a
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2018-01-31 23:04:38 +0100

    Add load config from file before env

--- parrot-api/config/configuration.go
+++ parrot-api/config/configuration.go
@@ -2,17 +2,27 @@ package config
 
 import (
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type AppConfig struct {
-	Port              string
-	DBName            string
-	DBConn            string
-	AuthIssuer        string
-	AuthSigningKey    string
+	Port           string `yaml:port`
+	DBName         string `yaml:dbName`
+	DBConn         string `yaml:dbConn`
+	AuthIssuer     string `yaml:authIssuer`
+	AuthSigningKey string `yaml:authSigningKey`
 }
 
-// TODO: add config from conf file function
+func FromYaml(data []byte) (*AppConfig, error) {
+	conf := &AppConfig{}
+	err := yaml.Unmarshal(data, conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
 func FromEnv() (*AppConfig, error) {
 	port, ok := os.LookupEnv("PARROT_API_PORT")
 	if !ok {
@@ -35,10 +45,10 @@ func FromEnv() (*AppConfig, error) {
 		authSigningKey = "secret"
 	}
 	return &AppConfig{
-		Port:              port,
-		DBName:            dbName,
-		DBConn:            dbConn,
-		AuthIssuer:        authIssuer,
-		AuthSigningKey:    authSigningKey,
+		Port:           port,
+		DBName:         dbName,
+		DBConn:         dbConn,
+		AuthIssuer:     authIssuer,
+		AuthSigningKey: authSigningKey,
 	}, nil
 }
